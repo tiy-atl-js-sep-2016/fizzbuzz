d45b1a4054887e725b75af13c5a44a446ba9cebc
Brit Butler <redline6561@gmail.com>
2016-10-03 14:15:07 -0400

    Link HTML and Javascript.

--- main.js
+++ main.js
@@ -8,14 +8,21 @@
 //   ....
 // }
 
-for (var count = 1; count <= 100; count++) {
-  if (count % 15 === 0) {
-    console.log("FizzBuzz");
-  } else if (count % 5 === 0) {
-    console.log("Buzz");
-  } else if (count % 3 === 0) {
-    console.log("Fizz");
-  } else {
-    console.log(count);
+var myName = "brit";
+
+var fizzBuzz = function (highest) {
+  console.log(myName);
+  for (var count = 1; count <= highest; count++) {
+    if (count % 15 === 0) {
+      console.log("FizzBuzz");
+    } else if (count % 5 === 0) {
+      console.log("Buzz");
+    } else if (count % 3 === 0) {
+      console.log("Fizz");
+    } else {
+      console.log(count);
+    }
   }
-}
+};
+
+fizzBuzz(20);
